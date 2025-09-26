a7af76852fcb21b316dd53f0bfe843ccb38284f2
Suthar Home <sutharhome@192.168.1.2>
2025-09-26 19:15:40 +0530

    Add First Program

---
+++ 01 First Program/first.c
@@ -0,0 +1,23 @@
+#include<stdio.h>//Header File for print function
+//necessary main function :- starting point of c program
+int main(void) {
+  //Print function for printing data (single line comment)
+  printf("Hello World!\n");//(;)semicolon after each statement ,\n for line break 
+  //format specifier
+  /*
+  for using literal in print function and scan function
+ ( Multi Line
+  Comment)
+   */
+   printf("Integers %d",10);
+   printf("Character %c",'A');
+   printf("Float %f",1.02345);
+  return 0;
+}
+
+/*
+Compiler in c:
+
+preprocessing -> compiling -> assembling -> linking
+
+*/
